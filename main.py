867fc7760c8db75a1653ea38b524109ab1ba8802
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2023-10-21 21:38:59 +0530

    riya said to updated

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import flet as ft
 
 
-def main(page: ft.Page):
+def main(page: ft.Page) -> None:
     page.title = "Welcome"
     page.vertical_alignment = "center"
     # page.horizontal_alignment = "center"
 
-    def close_dlg(e):
+    file_picker = ft.FilePicker()
+    page.overlay.append(file_picker)
+    page.update()
+
+    def close_dlg(e) -> None:
         dlg_modal.open = False
         page.update()
 
-    def yes_dlg(e):
+    def yes_dlg(e) -> None:
         dlg = ft.AlertDialog(
             title=ft.Text(value="Mails Send!"),
             on_dismiss=lambda e: print("Dialog dismissed!"),
@@ -29,29 +33,40 @@ def main(page: ft.Page):
         on_dismiss=lambda e: print("Modal dialog dismissed!"),
     )
 
-    def open_dlg_modal(e):
+    def open_dlg_modal(e) -> None:
         page.dialog = dlg_modal
         dlg_modal.open = True
         page.update()
 
+    def on_dialog_result(e) -> None:
+        pass
+
+    def generate_certificate(e) -> None:
+        pass
+
+    # All the btn
     btn_generate = ft.ElevatedButton(
-        text="Generate", icon=ft.icons.GENERATING_TOKENS_OUTLINED, color=ft.colors.GREEN
+        text="Generate",
+        icon=ft.icons.GENERATING_TOKENS_OUTLINED,
+        color=ft.colors.GREEN,
+        on_click=generate_certificate,
     )
+
+    # uploads btn
     btn_upload_certificate = ft.ElevatedButton(
         text="Upload certificate",
         icon=ft.icons.UPLOAD_FILE_OUTLINED,
         color=ft.colors.AMBER,
+        on_click=lambda _: file_picker.pick_files(allow_multiple=True),
     )
+
+    # send mail btn
     btn_send_mail = ft.ElevatedButton(
         text="Send Email",
         icon=ft.icons.SEND_ROUNDED,
         color=ft.colors.BLUE,
         on_click=open_dlg_modal,
     )
-
-    file_picker = ft.FilePicker()
-    page.overlay.append(file_picker)
-    page.update()
 
     # test
 
